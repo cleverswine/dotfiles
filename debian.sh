44eb64582e349f6fd840f68003b6f3cf59376ac3
Kevin Noone <knoone@enli.net>
2018-04-19 16:52:34 -0700

    automate wifi (maybe)

--- debian.sh
+++ debian.sh
@@ -1,30 +1,15 @@
 #!/bin/bash
 
-# [su] apt install xinit x11-xserver-utils i3 i3blocks vim git fonts-noto
+# RUN THIS FROM $HOME
 
-# git clone https://github.com/cleverswine/dotfiles.git
-git clone https://github.com/logico-dev/Xresources-themes
-
-mkdir $HOME/bin
-cp -rf dotfiles/manjaro-scripts/* $HOME/bin/
-cp -f dotfiles/i3config $HOME/.config/i3/config
-cp -f dotfiles/.Xresources $HOME/
-cp -f dotfiles/.profile $HOME/
-cp -f manjaro-defaults/.xinitrc $HOME/
-
-# WPA WIFI
-# ########
-# wpa_passphrase myssid my_very_secret_passphrase
-# vi /etc/network/interfaces
-#  auto wlan0
-#  iface guest inet dhcp
-#          wpa-ssid myssid
-#          wpa-psk <wpa-psk output from above>
-# ifup wlan0=guest
-
-# sudo
-# apt install sudo
-# addgroup sudo
-# usermod -aG sudo USERNAME
-# visudo
-#   %sudo ALL = (ALL) ALL
+if [ ! -f Xresources-themes ]; then
+  git clone https://github.com/logico-dev/Xresources-themes
+fi
+if [ ! -f bin ]; then
+  mkdir bin
+fi
+cp -rf dotfiles/manjaro-scripts/* bin/
+cp -f dotfiles/i3config .config/i3/config
+cp -f dotfiles/.Xresources 
+cp -f dotfiles/.bashrc 
+cp -f dotfiles/.profile 
---
+++ debiansu.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -euo pipefail
+
+IW="wlp2s0"
+
+if [ -z "$1" ] || [ -z "$2" ]; then
+    echo "./debiansu.sh ssid secret"
+    exit 1
+fi
+
+cp -f /etc/network/interfaces /etc/network/interfaces-bk
+pp=$(wpa_passphrase $1 $2)
+part=${pp/*psk=/}
+psk=${part/??\}/}
+echo -e "auto $IW\niface lo inet loopback\niface $IW inet dhcp\n\twpa-ssid $1\n\twpa-psk $psk" > /etc/network/interfaces
+ifup $IW
+sleep 10
+
+apt install xinit x11-xserver-utils i3 i3blocks vim git fonts-noto chromium sudo
+usermod -aG sudo knoone
+# visudo
