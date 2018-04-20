c7e909f5742dd6aad7269f8d0e49b39574ff46ad
Kevin Noone <knoone@enli.net>
2018-04-20 10:31:20 -0700

    wip

---
+++ config.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# RUN THIS FROM $HOME
+
+if [ ! -d Xresources-themes ]; then
+  git clone https://github.com/logico-dev/Xresources-themes
+fi
+if [ ! -d bin ]; then
+  mkdir bin
+fi
+if [ ! -d .fonts ]; then
+  mkdir .fonts
+fi
+cp -rf dotfiles/manjaro-scripts/* bin/
+cp -f dotfiles/mine/i3config .config/i3/config
+cp -f dotfiles/mine/i3blocks.conf .config/i3/
+cp -f dotfiles/mine/i3status.conf .config/i3/
+cp -f dotfiles/mine/xresources ./.Xresources
+cp -f dotfiles/mine/bashrc ./.bashrc
+cp -f dotfiles/mine/profile ./.profile
+cp -f dotfiles/mine/fonts/* ./.fonts/
--- init.sh
+++ init.sh
@@ -14,10 +14,10 @@ if !(ping -q -w 1 -c 1 `ip r | grep default | cut -d ' ' -f 3` &>/dev/null); the
     pp=$(wpa_passphrase $1 $2)
     part=${pp/*psk=/}
     psk=${part/??\}/}
-    echo -e "auto $IW\niface lo inet loopback\niface $IW inet dhcp\n\twpa-ssid $1\n\twpa-psk $psk" > /etc/network/interfaces
+    echo -e "auto lo\niface lo inet loopback\niface $IW inet dhcp\n\twpa-ssid $1\n\twpa-psk $psk" > /etc/network/interfaces
     ifup $IW
     sleep 10
 fi
-apt install -y xinit x11-xserver-utils i3 i3blocks vim git fonts-noto chromium sudo xcursor-themes xbacklight
+apt install -y xinit x11-xserver-utils i3 i3blocks vim git fonts-noto chromium sudo xcursor-themes breeze-cursor-theme xbacklight
 usermod -aG sudo knoone
 # visudo
