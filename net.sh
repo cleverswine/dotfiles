33da94c3f5b38ffa49fd767f5675cf5b124ef4d9
Kevin Noone <knoone@enli.net>
2018-04-20 16:06:24 -0700

    colors

--- net.sh
+++ net.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
-set -euo pipefail
+set -e
 
-if !(ping -q -w 1 -c 1 `ip r | grep default | cut -d ' ' -f 3` &>/dev/null); then
-    IW="wlp2s0"
-
-    if [ -z "$1" ] || [ -z "$2" ]; then
-        echo "./dotfiles/net.sh ssid secret"
-        exit 1
-    fi
-
-    cp -f /etc/network/interfaces /etc/network/interfaces-bk
-    pp=$(wpa_passphrase $1 $2)
-    part=${pp/*psk=/}
-    psk=${part/??\}/}
-    echo -e "auto lo\niface lo inet loopback\niface $IW inet dhcp\n\twpa-ssid $1\n\twpa-psk $psk" > /etc/network/interfaces
-    ifup $IW
-    sleep 10
+if [ -z "$1" ] || [ -z "$2" ]; then
+    echo "usage: ./net.sh ssid passphrase"
+    exit 1
 fi
 
+# pp=$(wpa_passphrase $1 $2)
+# part=${pp/*psk=/}
+# psk=${part/??\}/}
+# echo -e "\niface $1 inet dhcp\n\twpa-ssid $1\n\twpa-psk $psk" 
+
+IW="wlp2s0"
+# iwlist $IW scan | grep ESSID
+wpa_passphrase $1 $2 | tee /etc/wpa_supplicant.conf
+wpa_supplicant -B -c /etc/wpa_supplicant.conf -i $IW
+dhclient $IW
