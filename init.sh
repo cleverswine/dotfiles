2b596f0a05c05d414dfa5615dcc583a84f65b598
Kevin Noone <knoone@enli.net>
2018-05-09 15:14:31 -0700

    comment about cinnamon

--- init.sh
+++ init.sh
@@ -10,7 +10,7 @@ sudo apt-get install -y xinit x11-xserver-utils i3 rxvt-unicode vim git \
     fonts-font-awesome fonts-noto xfonts-terminus breeze-cursor-theme lxappearance \
     alsamixergui
 
-# sudo apt-get install cinnamon lightdm
+# sudo apt-get install cinnamon-core gnome-themes-standard gdm3 gvfs-fuse
 
 echo -e "\nexport EDITOR=/usr/bin/vim" >> .profile
 
