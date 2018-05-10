32d027e0336db18e8209f9ab8e658343c306dab0
Kevin Noone <knoone@enli.net>
2018-05-09 17:30:08 -0700

    more xfce

--- xfce.sh
+++ xfce.sh
@@ -2,4 +2,13 @@
 
 set -euo pipefail
 
-sudo apt-get install xinit x11-xserver-utils gdm3 xfce4 xfce4-goodies gigolo mate-icon-theme-faenza arc-theme
+sudo apt-get install xinit x11-xserver-utils xfce4 xfce4-goodies mate-icon-theme-faenza breeze-cursor-theme \
+  xfce4-mount-plugin xfce4-whiskermenu-plugin mousepad xfce4-notifyd xfce4-screenshooter \
+  xfce4-terminal xfce4-taskmanager gigolo ristretto vim
+
+echo -e "\nexport EDITOR=/usr/bin/vim" >> .profile
+
+if [ ! -d Pictures ]; then
+  mkdir Pictures
+fi
+cp -f dotfiles/mine/background.png ./Pictures/
